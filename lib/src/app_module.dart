6d46197a03498223a79f5845929c7dbc96c3b57c
Italo Zimbaldi <italo.zimbaldi@yahoo.com.br>
2023-06-24 21:29:11 -0300

    Aula 4 - tela de produtos

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -3,6 +3,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'modules/core/core_module.dart';
 import 'modules/login/login_module.dart';
 import 'modules/payment_type/payment_type_module.dart';
+import 'modules/products/products_module.dart';
 import 'modules/template/base_layout.dart';
 
 class AppModule extends Module {
@@ -19,10 +20,15 @@ class AppModule extends Module {
           child: (context, args) => const BaseLayout(
             body: RouterOutlet(),
           ),
+          transition: TransitionType.noTransition,
           children: [
             ModuleRoute(
               '/payment-type',
               module: PaymentTypeModule(),
+            ),
+            ModuleRoute(
+              '/products',
+              module: ProductsModule(),
             ),
           ],
         ),
---
+++ lib/src/repositories/products/product_repository_impl.dart
@@ -0,0 +1,99 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+import '../../core/exceptions/repository_exception.dart';
+import '../../core/rest_client/custom_dio.dart';
+import '../../models/product_model.dart';
+import 'dart:typed_data';
+
+import './product_repository.dart';
+
+class ProductRepositoryImpl extends ProductRepository {
+  final CustomDio _dio;
+
+  ProductRepositoryImpl(this._dio);
+
+  @override
+  Future<void> deleteProduct(int id) async {
+    try {
+      await _dio.auth().put(
+        '/products/$id',
+        data: {
+          'enabled': false,
+        },
+      );
+    } on DioException catch (e, s) {
+      log('Erro ao deletar produto', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao deletar produto');
+    }
+  }
+
+  @override
+  Future<List<ProductModel>> finldAll(String? name) async {
+    try {
+      final productResult = await _dio.auth().get(
+        '/products',
+        queryParameters: {
+          if (name != null) 'name': name,
+          'enabled': true,
+        },
+      );
+      return productResult.data
+          .map<ProductModel>((p) => ProductModel.fromMap(p))
+          .toList();
+    } on DioException catch (e, s) {
+      log('Erro ao buscar produtos', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao buscar produtos');
+    }
+  }
+
+  @override
+  Future<ProductModel> getProduct(int id) async {
+    try {
+      final productResult = await _dio.auth().get(
+            '/products/$id',
+          );
+      return ProductModel.fromMap(productResult.data);
+    } on DioException catch (e, s) {
+      log('Erro ao buscar produto $id', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao buscar produto $id');
+    }
+  }
+
+  @override
+  Future<void> save(ProductModel productModel) async {
+    try {
+      final client = _dio.auth();
+      final data = productModel.toMap();
+
+      if (productModel.id != null) {
+        await client.put('/products/$productModel.id', data: data);
+      } else {
+        await client.post('/products', data: data);
+      }
+    } on DioException catch (e, s) {
+      log('Erro ao salvar produto', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao buscar salvar');
+    }
+  }
+
+  @override
+  Future<String> uploadImageProduct(Uint8List file, String filename) async {
+    try {
+      final formData = FormData.fromMap(
+        {
+          'file': MultipartFile.fromBytes(file, filename: filename),
+        },
+      );
+      final response = await _dio.auth().post(
+            '/uploads',
+            data: formData,
+          );
+      return response.data['url'];
+    } on DioException catch (e, s) {
+      log('Erro ao fazer upload do arquivo', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao fazer upload do arquivo');
+    }
+  }
+}
